a42d0738325d9936f774e26003f30fe6f873dfcf
EvgenyaHramova <evgenya.hramova@yandex.ru>
2022-12-03 15:10:26 +0400

    izmenenie  folder Zadacha

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Написать программу, которая из имеющегося массива строк формирует массив из строк,
+длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры,
+либо задать на старте выполнения алгоритма.
+При решении не рекомендуется пользоваться коллекциями,
+лучше обойтись исключительно массивами.*/
+
+void StringArray(int m)
+{
+    string[] strArray = new string[m];
+
+    for (int i = 0; i < strArray.Length; i++)
+    {
+        Console.Write($"Введите с клавиатуры значение {i + 1} элемента массива, нажав после ввода клавишу ENTER:   ");
+        strArray[i] = Console.ReadLine()!;
+    }
+
+    Console.WriteLine($"\nСгенерирован следующий одномерный массив строк:");
+    Console.Write("[ ");
+    for (int i = 0; i < strArray.Length; i++)
+    {
+        Console.Write(strArray[i]);
+        if (i != strArray.Length - 1)
+        {
+            Console.Write(" ,  ");
+        }
+    }
+    Console.Write(" ]");
+    Console.WriteLine();
+}
+
+Console.Write("Введите количество элементов в одномерном массиве:  ");
+int m = Convert.ToInt32(Console.ReadLine());
+StringArray(m);
+
+
+
