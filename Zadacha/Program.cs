e92487db40e75cde0ce8f301415d21bcba29d66c
EvgenyaHramova <evgenya.hramova@yandex.ru>
2022-12-04 16:16:16 +0400

    izmenenie  folder Zadacha

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -4,17 +4,16 @@
 При решении не рекомендуется пользоваться коллекциями,
 лучше обойтись исключительно массивами.*/
 
-void StringArray(int m)
+void StringArray(string[] strArray)
 {
-    string[] strArray = new string[m];
-
     for (int i = 0; i < strArray.Length; i++)
     {
         Console.Write($"Введите с клавиатуры значение {i + 1} элемента массива, нажав после ввода клавишу ENTER:   ");
         strArray[i] = Console.ReadLine()!;
     }
 
-    Console.WriteLine($"\nСгенерирован следующий одномерный массив строк:");
+    Console.WriteLine($"\nПолучен следующий одномерный массив строк:");
+
     Console.Write("[ ");
     for (int i = 0; i < strArray.Length; i++)
     {
@@ -28,9 +27,70 @@ void StringArray(int m)
     Console.WriteLine();
 }
 
+void NewStringArray(string[] strArray, int countSimbol)
+{
+    string[] strArray1 = new string[strArray.Length];
+    int j = 0;
+
+    for (int i = 0; i < strArray.Length; i++)
+    {
+        if (strArray[i].Length <= countSimbol)
+        {
+            strArray1[j] = strArray[i];
+            //Console.Write(strArray1[j] + "   ");
+            j++;
+        }
+
+    }
+    //Console.WriteLine("j= " + j);
+
+    string[] newStrArray = new string[j];
+    int k = 0;
+    for (int i = 0; i < newStrArray.Length; i++)
+    {
+        if (strArray1[k] != "")
+        {
+            newStrArray[i] = strArray1[k];
+            k++;
+        }
+    }
+
+    Console.WriteLine($"\nНовый массив, состоящий из элементов, длина которых меньше либо равна {countSimbol} символа:");
+
+    Console.Write("[ ");
+    for (int i = 0; i < newStrArray.Length; i++)
+    {
+        Console.Write(newStrArray[i]);
+        if (i != newStrArray.Length - 1)
+        {
+            Console.Write(" ,  ");
+        }
+    }
+    Console.Write(" ]");
+    Console.WriteLine();
+}
+
 Console.Write("Введите количество элементов в одномерном массиве:  ");
 int m = Convert.ToInt32(Console.ReadLine());
-StringArray(m);
+string[] strArray = new string[m];
+StringArray(strArray);
+Console.Write("\nВведите предельное количество символов, которое может содержаться  в элементе нового массива:    ");
 
-
-
+int countSimbol = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < strArray.Length; i++)
+{
+    if (strArray[i].Length > countSimbol)
+    {
+        count++;
+    }
+}
+if (count == strArray.Length)
+{
+    Console.Write($"\nЭлементы в массиве с количеством символов {countSimbol} и менее отсутствуют.");
+}
+else
+{
+    NewStringArray(strArray, countSimbol);
+}
+Console.WriteLine();
